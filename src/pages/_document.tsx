ec911de2ed3c8d0839e78e84028ef437975e52db
xTirian <tirianx@yahoo.com>
2024-04-30 19:24:18 -0300

    feat: productCard component

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,6 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 class MyDocument extends Document {
-
   static async getInitialProps(ctx: any) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
@@ -10,7 +9,8 @@ class MyDocument extends Document {
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App: any) => (props: any) => sheet.collectStyles(<App {...props} />),
+          enhanceApp: (App: any) => (props: any) =>
+            sheet.collectStyles(<App {...props} />),
         });
 
       const initialProps = await Document.getInitialProps(ctx);
@@ -37,6 +37,10 @@ class MyDocument extends Document {
             content="Este site foi criado com o propósito de avaliar a proficiência dos desenvolvedores na elaboração de código claro, meticulosamente testado e passível de reutilização. Os participantes são solicitados a concluir uma tarefa específica e proceder com o deployment da aplicação, fornecendo os links correspondentes ao aplicativo e ao repositório associado. Esta avaliação possibilita uma análise abrangente do desempenho dos candidatos, sendo de suma importância para o desdobramento subsequente do processo avaliativo."
           />
           <meta name="author" content="xTirian - Matheus Fernandes" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+          />
           <link rel="icon" href="/logo.jpeg" />
         </Head>
         <body>
@@ -49,4 +53,3 @@ class MyDocument extends Document {
 }
 
 export default MyDocument;
-
