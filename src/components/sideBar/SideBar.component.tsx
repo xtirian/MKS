111ecc8ff876da7ae8ecd635c0da1504cddd8309
xTirian <tirianx@yahoo.com>
2024-04-30 15:42:41 -0300

    feat: add sidebar component

---
+++ src/components/sideBar/SideBar.component.tsx
@@ -0,0 +1,60 @@
+import { motion } from "framer-motion";
+import React from "react";
+import styles from "./SideBar.module.scss";
+import { IoMdClose } from "react-icons/io";
+import { useDispatch, useSelector } from "react-redux";
+import { SidebarContextProps } from "@/services/context/sidebar.context";
+import { RootState } from "@/services/reduxProvider";
+
+export const SideBar = () => {
+  const dispatch = useDispatch();
+  const toggleSidebar = () => {
+    dispatch({ type: "TOGGLE_SIDEBAR" });
+  };
+  const isOpen = useSelector((state: RootState) => state.sidebar.isOpen);
+
+  return (
+    <motion.div
+      className={styles.sideBarWraper}
+      initial="hidden"
+      animate={isOpen ? "visible" : "hidden"}
+      variants={sideBarAnimation}
+      transition={{
+        type: "spring",
+        damping: 10,
+        stiffness: 50,
+        mass: 0.5,
+      }}
+    >
+      <div className={styles.sideBarContainer}>
+        <header>
+          <h2>Carrinho de compras</h2>
+          <button type="button" onClick={() => toggleSidebar()}>
+            <IoMdClose />
+          </button>
+        </header>
+        <footer>
+          <p>Total:</p>
+          <p>R$ 0,00</p> {/* This value should be calculated */}
+        </footer>
+      </div>
+      <button className={styles.submitButton}>Finalizar Compra</button>
+    </motion.div>
+  );
+};
+const sideBarAnimation = {
+  hidden: {
+    x: "100%",
+    opacity: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,30 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
+
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/services/queryClient";
+import { AppProvider } from "@/services/reduxProvider";
+
 //import Header from '../components/Header';
-//import Sidebar from '../components/Sidebar';
+
+import "@/styles/global.scss";
+import { SideBar } from "@/components/sideBar/SideBar.component";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-          {/* <Header /> */}
-          {/* <Sidebar /> */}
-        <div>
+      <Head>
+        <title>MKS Challenge - xTirian</title>
+      </Head>
+      <AppProvider>
+        {/* <Header /> */}
+        <main>
+          <SideBar />
           <Component {...pageProps} />
-        </div>
+        </main>
+      </AppProvider>
     </QueryClientProvider>
   );
 }
 
 export default MyApp;
- 
